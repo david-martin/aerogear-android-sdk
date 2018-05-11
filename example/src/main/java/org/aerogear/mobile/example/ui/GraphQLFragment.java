794f1724bd3e5abb79cc26fd0a65fa92f7deceee
David Martin <david.martin@redhat.com>
2018-05-11 15:23:29 +0100

    Add caching

--- example/src/main/java/org/aerogear/mobile/example/ui/GraphQLFragment.java
+++ example/src/main/java/org/aerogear/mobile/example/ui/GraphQLFragment.java
@@ -1,8 +1,16 @@
 package org.aerogear.mobile.example.ui;
 
 import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.api.Operation;
 import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.api.ResponseField;
+import com.apollographql.apollo.cache.normalized.CacheKey;
+import com.apollographql.apollo.cache.normalized.CacheKeyResolver;
+import com.apollographql.apollo.cache.normalized.NormalizedCacheFactory;
+import com.apollographql.apollo.cache.normalized.lru.EvictionPolicy;
+import com.apollographql.apollo.cache.normalized.lru.LruNormalizedCacheFactory;
 import com.apollographql.apollo.cache.normalized.sql.ApolloSqlHelper;
+import com.apollographql.apollo.cache.normalized.sql.SqlNormalizedCacheFactory;
 import com.apollographql.apollo.exception.ApolloException;
 import com.apollographql.apollo.fetcher.ApolloResponseFetchers;
 import com.apollographql.apollo.subscription.WebSocketSubscriptionTransport;
@@ -21,6 +29,8 @@ import org.aerogear.mobile.example.R;
 import org.aerogear.mobile.example.type.Episode;
 
 import com.apollographql.apollo.ApolloClient;
+
+import java.util.Map;
 
 import javax.annotation.Nonnull;
 
@@ -54,42 +64,37 @@ public class GraphQLFragment extends BaseFragment {
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
             .build();
         ApolloSqlHelper apolloSqlHelper = new ApolloSqlHelper(this.getContext(), SQL_CACHE_NAME);
-//        NormalizedCacheFactory normalizedCacheFactory = new LruNormalizedCacheFactory(EvictionPolicy.NO_EVICTION)
-//            .chain(new SqlNormalizedCacheFactory(apolloSqlHelper));
-//
-//        CacheKeyResolver cacheKeyResolver = new CacheKeyResolver() {
-//            @Nonnull @Override
-//            public CacheKey fromFieldRecordSet(@Nonnull ResponseField field, @Nonnull Map<String, Object> recordSet) {
-//                String typeName = (String) recordSet.get("__typename");
-//                if ("User".equals(typeName)) {
-//                    String userKey = typeName + "." + recordSet.get("login");
-//                    return CacheKey.from(userKey);
-//                }
-//                if (recordSet.containsKey("id")) {
-//                    String typeNameAndIDKey = recordSet.get("__typename") + "." + recordSet.get("id");
-//                    return CacheKey.from(typeNameAndIDKey);
-//                }
-//                return CacheKey.NO_KEY;
-//            }
-//
-//            // Use this resolver to customize the key for fields with variables: eg entry(repoFullName: $repoFullName).
-//            // This is useful if you want to make query to be able to resolved, even if it has never been run before.
-//            @Nonnull @Override
-//            public CacheKey fromFieldArguments(@Nonnull ResponseField field, @Nonnull Operation.Variables variables) {
-//                return CacheKey.NO_KEY;
-//            }
-//        };
+        NormalizedCacheFactory normalizedCacheFactory = new LruNormalizedCacheFactory(EvictionPolicy.NO_EVICTION)
+            .chain(new SqlNormalizedCacheFactory(apolloSqlHelper));
+
+        CacheKeyResolver cacheKeyResolver = new CacheKeyResolver() {
+            @Nonnull @Override
+            public CacheKey fromFieldRecordSet(@Nonnull ResponseField field, @Nonnull Map<String, Object> recordSet) {
+                if (recordSet.containsKey("id")) {
+                    String typeNameAndIDKey = recordSet.get("__typename") + "." + recordSet.get("id");
+                    return CacheKey.from(typeNameAndIDKey);
+                }
+                return CacheKey.NO_KEY;
+            }
+
+            // Use this resolver to customize the key for fields with variables: eg entry(repoFullName: $repoFullName).
+            // This is useful if you want to make query to be able to resolved, even if it has never been run before.
+            @Nonnull @Override
+            public CacheKey fromFieldArguments(@Nonnull ResponseField field, @Nonnull Operation.Variables variables) {
+                return CacheKey.NO_KEY;
+            }
+        };
 
         apolloClient = ApolloClient.builder()
             .serverUrl(BASE_URL)
             .okHttpClient(okHttpClient)
-            //.normalizedCache(normalizedCacheFactory, cacheKeyResolver)
+            .normalizedCache(normalizedCacheFactory, cacheKeyResolver)
             .subscriptionTransportFactory(new WebSocketSubscriptionTransport.Factory(SUBSCRIPTION_BASE_URL, okHttpClient))
             .build();
 
         ApolloCall<ListHeroesQuery.Data> listHeroesQuery = apolloClient
             .query(new ListHeroesQuery(Episode.NEWHOPE))
-            .responseFetcher(ApolloResponseFetchers.NETWORK_ONLY);
+            .responseFetcher(ApolloResponseFetchers.CACHE_AND_NETWORK);
 
         listHeroesQuery.enqueue(new ApolloCall.Callback<ListHeroesQuery.Data>() {
             @Override
@@ -98,6 +103,7 @@ public class GraphQLFragment extends BaseFragment {
                 GraphQLFragment.this.getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
+                        heros.clear();
                         heros.add(response.data().hero());
                     }
                 });
